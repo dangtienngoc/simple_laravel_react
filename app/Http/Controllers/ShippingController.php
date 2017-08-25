d477a8f975e6a5078c229d08ee8326c17ac1f963
NgocDang <dangtienngoc@hotmail.com>
2017-08-25 09:20:59 +0700

    API post/get

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -2,22 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\ResponseFactory;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+
+use App\Shipping;
+
+use Validator;
 
 class ShippingController extends Controller {
-
 	public function index() {
-		$books = Book::paginate( 10 );
-		if ( ! $books ) {
+		$shipping = Shipping::all();
+
+		if ( ! $shipping ) {
 			throw new HttpException( 400, "Invalid data" );
 		}
 
-		return response()->json(
-			$books,
-			200
-		);
+		return response()->json( $shipping, 200 );
+	}
+
+
+	/**
+	 * @param Request $request
+	 *  Save Shipping to database
+	 *
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function store( Request $request ) {
+
+		// validate data before saving
+		$validator = Validator::make( $request->all(), [
+			'name'  => 'required|min:2|max:255',
+			'color' => 'required|min:2|max:255',
+			'cost'  => 'required',
+		] );
+
+		if ( $validator->fails() ) {
+			return response()->json( $validator->errors(), 400 );
+		}
+
+		$shipping = new Shipping;
+
+		$shipping->name = $request->name;
+		$shipping->color = $request->color;
+		$shipping->cost = $request->cost;
+
+		if ( $shipping->save() ) {
+			return $shipping;
+		}
+
+		throw new HttpException( 400, "Invalid data" );
 	}
 }
